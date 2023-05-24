04f312f109ff2e27df6926bb2c4eda56c7a1cb4c
Isaac J Moreira <120274547+IsaacJMoreira@users.noreply.github.com>
2023-05-24 17:52:30 -0300

    Função cadastrar receita concluida. FAZER: Adicionar função salvar receita

--- receitasOmma/index.js
+++ receitasOmma/index.js
@@ -11,10 +11,9 @@ let templateReceitas = { // Cria um ojeto em branco com a estrutura
                          //de como devem ser organizados os dados
     titulo:"",
     dificuldade:"",
-    ingredientes: [// Array para indexar os ingredientes
-        //vazio para podermos adicionar mais objetos aqui dentro
-    ],
-    etapas: [], // Array simples para armazenar passos (strings)
+    ingredientes: listaDeIngredientes = [],// Array para indexar os ingredientes
+        //vazio para podermos adicionar mais objetos aqui dentro,
+    etapas:listaDeEtapas = [], // Array simples para armazenar passos (strings)
     linkVideo: "",
     vegano: ""
 };
@@ -28,15 +27,11 @@ let templateIngrediente = {
 //Ok, let the fun begin
 
 //O programa vai perguntar e pedir validação inúmeras vezes. Melhor tornar esse processo uma função :)
-function pergunta (mensagem){
-    let resposta;
+function confirmacao (mensagem){
     let continuar;
 
     do{
-        resposta = readlineSync.question(mensagem);
-
-        do{
-            continuar = readlineSync.question(`Voce digitou "${resposta}". Deseja salvar? (S/N)`);
+            continuar = readlineSync.question(`${mensagem} (S/N)`);
             switch(continuar){
                 case "S":
                 case "s":
@@ -57,9 +52,7 @@ function pergunta (mensagem){
                     continuar = "invalid";
             }
         }while(continuar === "invalid");
-    }while(continuar);
-
-    return resposta;
+    return continuar;
 }
 
 function cadastrarReceita (){//função responsável pelo cadastro das informações da receita.
@@ -71,25 +64,46 @@ function cadastrarReceita (){//função responsável pelo cadastro das informaç
     let Receita = templateReceitas; //Cria um objeto vazio para armazenar os dados. Ele é temporário.
     let ingrediente = templateIngrediente;
 
-    Receita.titulo = pergunta("Qual o titulo da receita a ser cadastrada? ");
+    Receita.titulo = readlineSync.question("Qual o titulo da receita a ser cadastrada? ");
     //document.write("Título: " + Receita.titulo + "<br>");
-    Receita.dificuldade = pergunta("Qual a dificuldade desta receita? ");
+    Receita.dificuldade = readlineSync.question("Qual a dificuldade desta receita? ");
     //document.write("Dificuldade: " + Receita.dificuldade + "<br>");
     
-    let maisIngredientes;
+    let adicionarMais;
 
-    console.log("Agora vamos cadastrar os ingredientes!");
+    console.log("\nAgora vamos cadastrar os ingredientes!");//aqui, adicionamos ingredientes
     do{
-        ingrediente.nome = pergunta("Qual o nome do ingrediente? ");
-        ingrediente.quantidade = pergunta("Qual a quantidade a ser utilizada? ");
-        ingrediente.unidade = pergunta("Como este ingrediente e medido? (xicaras, gramas, quilos, etc) ");
+        ingrediente.nome = readlineSync.question("\nQual o nome do ingrediente?\n");
+        ingrediente.quantidade = readlineSync.question("Qual a quantidade a ser utilizada? ");
+        ingrediente.unidade = readlineSync.question("Como este ingrediente e medido? (xicaras, gramas, quilos, etc) ");
         
         Receita.ingredientes.push(ingrediente);
-        maisIngredientes = pergunta("Cadastrar mais ingredientes? "); 
+        adicionarMais = confirmacao("Cadastrar mais ingredientes? "); 
         
-    }while(maisIngredientes === "S");
+    }while(adicionarMais === false);
 
-    bancoDeReceitas.push(Receita);
+    let etapa;
+
+    console.log("\nAgora vamos cadastrar os passos da receita!\n");
+
+    etapa = readlineSync.question("Qual o primeiro passo da receita?\n"); 
+    Receita.etapas.push(etapa);
+    adicionarMais = confirmacao("Cadastrar mais passos? ");
+
+    while(adicionarMais === false){
+        etapa = readlineSync.question("Qual o proximo passo da receita?\n"); 
+        Receita.etapas.push(etapa);
+        adicionarMais = confirmacao("Cadastrar mais passos? ");         
+    }
+
+    ingrediente.linkVideo = readlineSync.question("Adicione um link para o video da receita. Caso nao exista, digite \"N/A\"\n");
+    ingrediente.vegano = readlineSync.question("Esta receita e vegana? (S/N) ");
+
+    if(confirmacao("Salvar receita? ") === false){
+         bancoDeReceitas.push(Receita);
+         console.log("\n >>> RECEITA SALVA COM SUCESSO! <<<\n");
+    }else console.log("\n >>> RECEITA NÃO SALVA! <<<\n");
+
     
     
 }
