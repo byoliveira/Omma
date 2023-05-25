08733094e3fd204111ccded3e82ca26af099a7fc
Isaac J Moreira <120274547+IsaacJMoreira@users.noreply.github.com>
2023-05-24 23:50:15 -0300

    Programa quase finalizado. PROBLEMA:  FAZER:

--- receitasOmma/index.js
+++ receitasOmma/index.js
@@ -1,5 +1,5 @@
 // Constantes do programa
-const programName = "Omma - Serviços em Gastronomia LTDA"
+const companyName = "Omma - Serviços em Gastronomia LTDA"
 var readlineSync = require('readline-sync');
 // const prompt = require('prompt-sync')({sigint: true});
 
@@ -11,18 +11,20 @@ let templateReceitas = { // Cria um ojeto em branco com a estrutura
                          //de como devem ser organizados os dados
     titulo:"",
     dificuldade:"",
-    ingredientes: listaDeIngredientes = [],// Array para indexar os ingredientes
+    ingredientes: [],// Array para indexar os ingredientes
         //vazio para podermos adicionar mais objetos aqui dentro,
     etapas:listaDeEtapas = [], // Array simples para armazenar passos (strings)
     linkVideo: "",
     vegano: ""
 };
 
-let templateIngrediente = {
-    quantidade: 0,//deve ser expresso em número
-    unidade: "",//xícara, colher de sopa, Kg, etc
-    nome: ""//nome do ingrediente
-};
+//Beleza, vamos usar essa idéia depois
+
+// let templateIngrediente = {
+//     quantidade: 0,//deve ser expresso em número
+//     unidade: "",//xícara, colher de sopa, Kg, etc
+//     nome: ""//nome do ingrediente
+// };
 
 //Ok, let the fun begin
 
@@ -58,14 +60,14 @@ function confirmacao (mensagem){
 
 //função responsável pelo cadastro das informações da receita.
 
-function cadastrarReceita (){
+function cadastrarReceita (banco){
     console.log("***********************************************");
     console.log("*            CADASTRANDO RECEITA              *");
     console.log("***********************************************");
     //document.write("CADASTRANDO RECEITA<br><br>");
 
     let Receita = templateReceitas; //Cria um objeto vazio para armazenar os dados. Ele é temporário.
-    let ingrediente = templateIngrediente;
+   
 
     Receita.titulo = readlineSync.question("Qual o titulo da receita a ser cadastrada? ");
     //document.write("Título: " + Receita.titulo + "<br>");
@@ -74,12 +76,12 @@ function cadastrarReceita (){
     
     let adicionarMais;//meio confuso, talvez mudar o nome para naoAdicionarMais?
 
+    let ingrediente;//variável responsável por guardar os ingredientes temporariamente.
+
     console.log("\nAgora vamos cadastrar os ingredientes!");//aqui, adicionamos ingredientes
     do{
-        ingrediente.nome = readlineSync.question("\nQual o nome do ingrediente?\n");
-        ingrediente.quantidade = readlineSync.question("Qual a quantidade a ser utilizada? ");
-        ingrediente.unidade = readlineSync.question("Como este ingrediente e medido? (xicaras, gramas, quilos, etc) ");
-        
+        ingrediente = readlineSync.question("\nQual o nome do ingrediente?\n");
+               
         Receita.ingredientes.push(ingrediente);
         adicionarMais = confirmacao("Cadastrar mais ingredientes? "); 
         
@@ -99,10 +101,10 @@ function cadastrarReceita (){
         adicionarMais = confirmacao("Cadastrar mais passos? ");         
     }
 
-    ingrediente.linkVideo = readlineSync.question("Adicione um link para o video da receita. Caso nao exista, digite \"N/A\"\n");
-    ingrediente.vegano = readlineSync.question("Esta receita e vegana? (S/N) ");
+    Receita.linkVideo = readlineSync.question("Adicione um link para o video da receita. Caso nao exista, digite \"N/A\"\n");
+    Receita.vegano = readlineSync.question("Esta receita e vegana? (S/N) ");
     
-    salvarReceita(Receita, bancoDeReceitas);
+    salvarReceita(Receita, banco);
     
 }
 
@@ -113,7 +115,75 @@ function salvarReceita(receita, banco){
     }else console.log("\n >>> RECEITA NÃO SALVA! <<<\n"); 
 }
 
-cadastrarReceita();
-console.log(bancoDeReceitas);
-//document.write(bancoDeReceitas);
+function deletarReceita(banco){
+    let element = readlineSync.question("Qual o índice da receita a ser deletada?\n"); 
+    
+    if(element < banco.length){
+        if(confirmacao("Deletar receita? ") === false){
+            banco.splice(element, 1);
+            console.log(`\n >>> RECEITA ${element} DELETADA COM SUCESSO! <<<\n`);
+       }else console.log("\n >>> OPERACAO ABORTADA! <<<\n"); 
+    }else{
+        console.log(`\n >>> RECEITA '\${elemente}'\ NAO ENCONTRADA OU INEXISTENTE! <<<\N`);
+    }
+
+
+       
+}
+
+function exibirReceita(banco){
+    banco.forEach(element => {
+        console.log(element);
+    });    
+}
+
+//OK, daqui pra frente vamos criar a interface de interação com o usuário
+
+//Vamos mostrar o nome da empresa:
+console.log("***********************************************");
+console.log(`     ${companyName}       `);
+console.log("***********************************************\n");
+console.log(`Bom vindo(a) ao gerenciador de receitas da\n ${companyName}\n`);
+
+let continuarLoop;
+do{
+    console.log("***********************************************\n")
+    console.log("Para exibir as receitas cadastradas, digite \"exibir\"");
+    console.log("Para cadastrar uma nova receita, digite \"cadastrar\"");
+    console.log("Para deletar uma receita, digite \"deletar\"");
+    console.log("Para finalizar o programa, digite \"sair\"");
+    let fazer = readlineSync.question("\nOmma> ");
+    switch(fazer){
+        case "exibir":
+        case "EXIBIR":
+        case "Exibir":
+            exibirReceita(bancoDeReceitas);
+            continuarLoop = true;
+            break;
+        case "cadastrar":
+        case "Cadastrar":
+        case "CADASTRAR":
+            cadastrarReceita(bancoDeReceitas);
+            continuarLoop = true;
+            break;
+        case "deletar":
+        case "Deletar":
+        case "DELETAR":
+                deletarReceita(bancoDeReceitas);
+                continuarLoop = true;
+                break;
+        case "sair":
+        case "Sair":
+        case "SAIR":
+        case "exit":
+                console.log(`\N >>> OBRIGADO POR UTILIZAR O EDITOR DE RECEITAS DA ${companyName}\n`);
+                continuarLoop = false;
+                break;
+        default:
+            console.log(`\n >>> O COMANDO \"${fazer}\" NAO E VALIDO, TENTE NOVAMENTE <<<\n`);
+
+    }
+
+}while(continuarLoop);
+
 
